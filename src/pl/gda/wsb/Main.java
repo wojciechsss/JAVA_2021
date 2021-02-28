830c950bfa35be2b4dcc815b0efc7eb566727048
Wojciech Siedlecki <cezarek.w@wp.pl>
2021-02-28 16:22:50 +0100

    update class: Human Animal Phone

--- src/pl/gda/wsb/Main.java
+++ src/pl/gda/wsb/Main.java
@@ -6,14 +6,17 @@ public class Main {
         System.out.println("Hello World!!");
 
         Animal animal1 = new Animal("mouse", "Jerry", 0.8);
+        Animal animal2 = new Animal("cat", "Tom", 4.5);
         System.out.println(animal1);
         System.out.println(animal1.name);
 
 
         Human human1 = new Human("Wojtek", "Siedlecki",24);
+        Human human2 = new Human("Darek", "Adacki",15);
         System.out.println(human1.age);
 
         Phone phone1 = new Phone("A10", "Samsung", 123654789);
+        Phone phone2 = new Phone("Note 8T", "Redmi", 987654321);
         System.out.println(phone1.number);
     }
 }
